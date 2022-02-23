12d9248069cf6b364fcbb062e4cf990dbd5e848e
tgtiburon <tg.tiburon@gmail.com>
2022-02-22 19:01:06 -0500

    switch working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-
+import React from "react";
 
 import {
   ApolloClient, // constructor function to help initialize connection to GraphQL API server
-  InMemoryCache,// cache api response data to perform requests efficiently
+  InMemoryCache, // cache api response data to perform requests efficiently
   ApolloProvider, // Provides data to other components
-  createHttpLink,// middleware for outbound network requests
-} from '@apollo/client';
+  createHttpLink, // middleware for outbound network requests
+} from "@apollo/client";
 
+// renamed it as Router to be easier to work with
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
-import Home from './pages/Home';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+import Home from "./pages/Home";
 
-// Link to GraphQL 
+// Link to GraphQL
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Instantiate ApolloClient and connect to the graphql
@@ -27,20 +33,29 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
-// JSX tags will have acess to the server's api data through the 
+// JSX tags will have access to the server's api data through the
 // client we setup
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/thought" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-      </ApolloProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
